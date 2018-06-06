976db82da282b7d2c5b7da00b3632bbad3700916
AleksejevaTatjana <skvorcova.tatjana@gmail.com>
2018-06-06 15:43:34 +0300

    Message

--- src/test/java/delfiTests/FirstHomeWorkTest.java
+++ src/test/java/delfiTests/FirstHomeWorkTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class firstHomeWorkTest {
+public class FirstHomeWorkTest {
     private final String HOME_PAGE = "http://rus.delfi.lv/";
     private final By LINK_ELEMENT_SINOPTIKI = By.linkText("Синоптики обещают: к концу недели в Латвии станет теплее");
 
