99b790fb19394beab582f84d5dce85b82f532862
AleksejevaTatjana <skvorcova.tatjana@gmail.com>
2018-06-14 13:07:11 +0300

    First Home Work

--- src/test/java/delfiTests/FirstHomeWorkTest.java
+++ src/test/java/delfiTests/FirstHomeWorkTest.java
@@ -1,20 +1,35 @@
 package delfiTests;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
 
 public class FirstHomeWorkTest {
     private final String HOME_PAGE = "http://rus.delfi.lv";
-    private final By LINK_ELEMENT_SINOPTIKI = By.linkText("Синоптики обещают: к концу недели в Латвии станет теплее");
+    private final String ARTICLE_NAME = "Четыре желтые буквы. Как IKEA изменит жизнь в Латвии";
+    private final By ARTICLE_PATH = By.xpath(".//a[@href=\"http://www.delfi.lv/biznes/bizopinion/chetyre-zheltye-bukvy-kak-ikea-izmenit-zhizn-v-latvii.d?id=50121303\"]");
 
     @Test
-    public void sinoptikiExistTest() {
+    public void сheckArticleAtHomePageTest() {
         System.setProperty("webdriver.gecko.driver", "C:/Java/geckodriver/geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get(HOME_PAGE);
-        driver.findElement(LINK_ELEMENT_SINOPTIKI);
+
+        List<WebElement> articleList = driver.findElements(ARTICLE_PATH);
+
+        boolean isArticleExist = false;
+        for (int i = 0; i < articleList.size(); i++) {
+            if (articleList.get(i).getText().equals(ARTICLE_NAME)) {
+                isArticleExist = true;
+                break;
+            }
+        }
+        Assert.assertTrue("This article is not found!", isArticleExist);
     }
 }
