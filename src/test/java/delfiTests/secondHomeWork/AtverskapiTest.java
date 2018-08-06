3ca7310d33ab41dc55f994f52c9e67da76785b5b
AleksejevaTatjana <skvorcova.tatjana@gmail.com>
2018-08-06 17:37:33 +0300

    second homework

--- src/test/java/delfiTests/secondHomeWork/AtverskapiTest.java
+++ src/test/java/delfiTests/secondHomeWork/AtverskapiTest.java
@@ -16,12 +16,12 @@ public class AtverskapiTest {
     private final By DROPDOWN_MENU_ITEMS = By.xpath("//a[@class='sitemap-link']");
     private final By DROPDOWN_MENU_WOMAN = By.xpath("//a[@class='dropdown-toggle']");
     private final By WOMAN_DROPDOWN_MENU_ITEM = By.xpath("//ul[@class='dropdown-menu']/li");
-    private final By CHECKBOX_ITEM = By.xpath("//div[@class='step3-container']/div");
-    private final By CHECKBOX_CONDITION = By.xpath("//div[@class='']/label");
-    private final By COLOR_FILTER = By.xpath("//div[@class='col-xs-2 filter-colors-item']");
+    private final By CHECKBOX_ITEM = By.xpath("//div[@class='step3-container']/div/label");
+    private final By CHECKBOX_CONDITION = By.xpath("//div[@class='filters-content filter-condition']/div/label");
+    private final By COLOR_FILTER = By.xpath("//span[@class='glyphicon glyphicon-ok']");
     private final By REGION_DROPDOWN_BTN = By.name("district");
     private final By REGION_REGION_NAME = By.xpath("//div[@class='filters-content']/select/option");
-   // private final By FILTRET_BTN = By.xpath("//button[@class='btn btn-block btn-blue']");
+    // private final By FILTRET_BTN = By.xpath("//button[@class='btn btn-block btn-blue']");
 
     @Test
     public void swimwearFilterTest() {
@@ -35,15 +35,10 @@ public class AtverskapiTest {
         selectDropdownMenuItem("Atverskapi");
         selectWomanDropdownBtn("Sievietēm");
         selectWomanDropdownMenuItem("Apakšveļa / Peldkostīmi");
-
         selectCheckboxItem("Peldkostīmi", CHECKBOX_ITEM);
-        // waitLoadCheckbox();
-
         selectCheckboxCondition("Jauns", CHECKBOX_CONDITION);
-        // waitLoadCheckbox();
 
         selectColorFilter("Tumši sarkana", COLOR_FILTER);
-        // waitLoadColor();
 
         driver.findElement(REGION_DROPDOWN_BTN).click();
         selectRegionFilterItem("Rīgas rajons", REGION_REGION_NAME);
@@ -87,9 +82,7 @@ public class AtverskapiTest {
     }
 
     private void selectCheckboxItem(String filterName, By locator) {
-
         List<WebElement> filterItems = driver.findElements(locator);
-
         for (WebElement filterItem : filterItems) {
             if (filterItem.getText().contains(filterName)) {
                 filterItem.click();
@@ -98,24 +91,27 @@ public class AtverskapiTest {
         }
     }
 
-    private void selectCheckboxCondition(String filterName, By locator) {
-        List<WebElement> filterConditions = driver.findElements(locator);
-        for (WebElement filterCondition : filterConditions) {
-            if (filterCondition.getText().contains(filterName)) {
-                filterCondition.click();
-            }
-        }
-    }
-    private void selectColorFilter(String filterName, By locator) {
+    private void selectColorFilter(String colorName, By locator) {
         List<WebElement> filterColors = driver.findElements(locator);
-        for (WebElement filterColor : filterColors) {
-            if (filterColor.getText().contains(filterName)) {
+                for (WebElement filterColor : filterColors) {
+            if (filterColor.getText().contains(colorName)) {
                 filterColor.click();
+                break;
             }
         }
     }
 
-        private void selectRegionFilterItem(String regionName, By locator) {
+    private void selectCheckboxCondition(String filterName, By locator) {
+        List<WebElement> filterConditions = driver.findElements(locator);
+        for (WebElement filterCondition : filterConditions) {
+            if (filterCondition.getText().equals(filterName)) {
+                filterCondition.click();
+                break;
+            }
+        }
+    }
+
+    private void selectRegionFilterItem(String regionName, By locator) {
         List<WebElement> dropdownRegionList = driver.findElements(locator);
         driver.findElement(REGION_REGION_NAME).click();
 
@@ -127,12 +123,5 @@ public class AtverskapiTest {
         }
     }
 
-    // private void waitLoadCheckbox() {
-    //      new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(CHECKBOX_FILTER));
-    //   }
-
-    // private void waitLoadColor() {
-    //     new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(COLOR_FILTER));
-    // }
 }
 
