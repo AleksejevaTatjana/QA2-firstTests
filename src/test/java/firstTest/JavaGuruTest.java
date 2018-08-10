157ffe8d27776aaaec70d2c9eb511ff96cf735f9
AleksejevaTatjana <skvorcova.tatjana@gmail.com>
2018-08-10 11:04:01 +0300

    JavaGuruTest

--- src/test/java/firstTest/JavaGuruTest.java
+++ src/test/java/firstTest/JavaGuruTest.java
@@ -6,33 +6,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class JavaGuruTest {
-    private static final By QA_COURSE_MENU_LINK = By.xpath(".//[text()='Курс Тустирования(QA)']");
-    private static  final By ERROR_FIELD_TXT = By.xpath(".//div[text()=Šis ir obligāts jauntājums.']");
-    private static final By QA_REGISTRATION_BTN = By.className("regCourseA");
-    private static final By SEND_BTN = By.id("ss-submit");
-    private static final By NAME_FIELD = By.id("entry_1000000");
-    private String HOME_PAGE = "http://javaguru.lv";
+    private final String COURSE_NAME = "Курс PHP";
+    private final String HOME_PAGE = "http://javaguru.lv";
+    private final By COURSE_ITEM = By.xpath(".//*[@class = 'menuAcourse']/span");
 
     @Test
-    private void javaGuruReqTest() {
-        System.setProperty("webdriver.gecko.driver", "C:/Java/geckodriver/geckodriver.exe");
+    public void checkElementInMenu() {
+        System.setProperty("webdriver.gecko.driver", "c:/geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get(HOME_PAGE);
-        driver.findElement(QA_COURSE_MENU_LINK).click();
-        driver.findElement(QA_REGISTRATION_BTN).click();
-        ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
-        while (tabs.size() != 2) {
-            tabs.clear();
-            tabs.addAll(driver.getWindowHandles());
+
+        List<WebElement> menuItems = new ArrayList<WebElement>();
+        menuItems = driver.findElements(COURSE_ITEM);
+
+        boolean isCourseFound = false;
+        for(int i = 0; i < menuItems.size(); i++) {
+            if (menuItems.get(i).getText().equals(COURSE_NAME)) {
+                isCourseFound = true;
+                break;
+            }
         }
-        driver.switchTo().window(tabs.get(1));
-        driver.findElement(NAME_FIELD).sendKeys("Name");
-        driver.findElement(SEND_BTN).click();
-        Assert.assertTrue("No Error message")", driver.findElements(ERROR_FIELD_TXT).size()>0);
+
+        Assert.assertTrue("No menu Item is found!", isCourseFound);
     }
 }
